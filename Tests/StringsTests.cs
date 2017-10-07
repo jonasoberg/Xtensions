0881af1c543ee6cf3ae89e0d175ac0581716e35d
jonasoberg <jonas.oberg@gmail.com>
2017-10-07 17:06:37 +0200

    Add testproject

---
+++ Tests/StringsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xtensions;
+using System.Collections.Generic;
+
+namespace Tests {
+    [TestClass]
+    public class StringsTests {
+        [TestMethod]
+        public void Test_String_ToInt32() {
+
+            var tests = new List<Tuple<string, int>>();
+            tests.Add(new Tuple<string, int>("1", 1));
+            tests.Add(new Tuple<string, int>("-1", -1));
+            tests.Add(new Tuple<string, int>("123", 123));
+
+            tests.Add(new Tuple<string, int>("hello", 0));
+            tests.Add(new Tuple<string, int>("", 0));
+            tests.Add(new Tuple<string, int>(null, 0));
+
+
+            tests.ForEach(t => Assert.IsTrue(t.Item1.ToInt32() == t.Item2, $"could not parse string '{t.Item1}' to int32: {t.Item2}"));
+
+        }
+    }
+}

9a5841c41f017a11676b7972f9b718e8f84ec6fa
jonasoberg <jonas.oberg@gmail.com>
2017-10-07 16:46:22 +0200

    init project

---
+++ Xtensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Xtensions
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string self, int defaultValue = 0) {
+            if(String.IsNullOrEmpty(self)) {
+                return defaultValue;
+            }
+            int ret = defaultValue;
+            if(int.TryParse(self, out ret)) {
+                return ret;
+            } else {
+                return defaultValue;
+            }
+        }
+    }
+}

5e8963b91e3e521532ab4518861c7ac55ca90874
jonasoberg <jonas.oberg@gmail.com>
2017-10-24 23:49:20 +0200

    Test_XmlDocument_AddXmlNodes test

---
+++ Tests/XmlDocumentsTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Xtensions.XmlDocuments;
+namespace Tests {
+    [TestFixture]
+    public class XmlDocumentsTests {
+
+        [Test]
+        public void Test_XmlDocument_AddXmlDeclaration() {
+            var x = new XmlDocument();
+            var y = x.AddXmlDeclaration();
+
+            Assert.AreEqual(y.OuterXml, x.OuterXml);
+
+            var root = x.AddChildElement("data");
+            root.AddChildElement("e1").InnerText = "test1";
+            root.AddChildElement("e2").InnerText = "test2";
+            root.AddChildElement("e3").InnerText = "test3";
+            root.AddChildElement("e4").InnerText = "test4";
+
+            var m_OuterXml = x.OuterXml;
+
+            /*
+             <?xml version="1.0" encoding="UTF-8" standalone="true"?>
+                <data>
+                    <e1>test1</e1>
+                    <e2>test2</e2>
+                    <e3>test3</e3>
+                    <e4>test4</e4>
+                </data>
+            */
+            var xml =  XElement.Parse(m_OuterXml);
+
+            var actual = xml.Descendants().Where(m => m.Name.LocalName == "e3").Select(m => m.Value).SingleOrDefault();
+            Assert.AreEqual("test3", actual);
+        }
+    }
+}
---
+++ Xtensions/XmlDocuments/XmlDocumentsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Xtensions.XmlDocuments
+{
+    public static class XmlDocumentsExtensions
+    {
+        public static XmlDocument AddXmlDeclaration(this XmlDocument self, string version = "1.0", string encoding = "utf-8", string standalone = "yes") {
+            var decl = self.CreateXmlDeclaration(version, encoding, standalone);
+            self.AppendChild(decl);
+            return self;
+        }
+        public static XmlNode AddChildElement(this XmlNode self, string elementName) {
+            var e = self.OwnerDocument == null ? ((XmlDocument)self).CreateElement(elementName) : self.OwnerDocument.CreateElement(elementName);
+            self.AppendChild(e);
+            return e;
+        }
+    }
+}

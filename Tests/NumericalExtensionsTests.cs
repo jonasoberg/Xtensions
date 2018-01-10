8f05374f5b7662389dee1ec140cdad18dee06407
jonas.oberg <jonas@genero.se>
2018-01-10 09:11:33 +0100

    numerical isbetween

---
+++ Tests/NumericalExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xtensions.Numerical;
+namespace Tests {
+    [TestFixture]
+    public class NumericalExtensionsTests {
+        [Test]
+        public void Test_IsBetween(){
+
+            Assert.AreEqual(true, 5.IsBetween(0, 10));
+            Assert.AreEqual(false, 50.IsBetween(0, 10));
+            Assert.AreEqual(true, 5.IsBetween(0, 5));
+
+            Assert.AreEqual(true, ((int?)5).IsBetween(0, 10));
+            Assert.AreEqual(false, ((int?)50).IsBetween(0, 10));
+            Assert.AreEqual(false, ((int?)null).IsBetween(0, 10));
+        }
+    }
+}
---
+++ Xtensions/Numerical/NumericalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xtensions.Numerical
+{
+    public static class NumericalExtensions
+    {
+        public static bool IsBetween(this int? value, int start, int end) {
+            if(value.HasValue == false) {
+                return false;
+            }
+
+            return value.Value.IsBetween(start, end);
+        }
+        public static bool IsBetween(this int value, int start, int end) {
+            return Comparer<int>.Default.Compare(value, start) >= 0 && Comparer<int>.Default.Compare(value, end) <= 0;
+        }
+    }
+}
